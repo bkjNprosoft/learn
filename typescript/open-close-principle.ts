b12c42bc0a5f6ac87129f0d700a59901b3222c83
부경준 <bookj@nprosoft.kr>
2023-10-04 11:57:25 +0900

    add: util-number, typescript files namespace append

--- typescript/open-close-principle.ts
+++ typescript/open-close-principle.ts
@@ -1,53 +1,57 @@
-class Animal {
-  name: string;
+namespace OpenClosePrinciple {
 
-  attack() {
-    console.log(this.name, 'Attack!!');
-  };
-}
+  class Animal {
+    name: string;
 
-class Dog extends Animal{
-  constructor() {
-    super();
-    this.name = 'Dog';
+    attack() {
+      console.log(this.name, 'Attack!!');
+    };
   }
 
-  attack() {
-    return new Promise((resolve, reject) => {
+  class Dog extends Animal {
+    constructor() {
+      super();
+      this.name = 'Dog';
+    }
+
+    attack() {
+      return new Promise((resolve, reject) => {
+        super.attack();
+        resolve(true);
+      })
+    }
+  }
+
+  class Bird extends Animal {
+    constructor() {
+      super();
+      this.name = 'Bird';
+    }
+
+    attack() {
       super.attack();
-      resolve(true);
-    })
+    }
   }
+
+  class AnimalAttacker {
+    animal: Animal;
+
+    constructor(animal: Animal) {
+      this.animal = animal;
+    }
+
+    attack() {
+      this.animal.attack();
+    }
+  }
+
+  const dog = new Dog();
+  const bird = new Bird();
+
+  const dogAttacker = new AnimalAttacker(dog);
+  const birdAttacker = new AnimalAttacker(bird);
+
+  dogAttacker.attack();
+  birdAttacker.attack();
+
 }
-
-class Bird extends Animal{
-  constructor() {
-    super();
-    this.name = 'Bird';
-  }
-
-  attack() {
-    super.attack();
-  }
-}
-
-class AnimalAttacker {
-  animal: Animal;
-  
-  constructor(animal: Animal) {
-    this.animal = animal;
-  }
-
-  attack() {
-    this.animal.attack();
-  }
-}
-
-const dog = new Dog();
-const bird = new Bird();
-
-const dogAttacker = new AnimalAttacker(dog);
-const birdAttacker = new AnimalAttacker(bird);
-
-dogAttacker.attack();
-birdAttacker.attack();
---
+++ typescript/util-number.ts
@@ -0,0 +1,18 @@
+namespace UtilNumber {
+
+  function floor(num: number, fixed: number): number {
+    if (num == null || fixed == null) {
+      return undefined;
+    }
+
+    let fixedNum = 1;
+    for (let i = 0; i < fixed; ++i) {
+      fixedNum *= 10;
+    }
+
+    return Math.floor(num * fixedNum) / fixedNum;
+  }
+  console.log(floor(0.2193545, 2));
+
+}
+
