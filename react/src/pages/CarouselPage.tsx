fe1a9f54d27358b2b85e6f730e32da27b905fe93
bkjNprosoft <bookj.nprosoft@gmail.com>
2025-01-20 19:00:20 +0900

    modify: [react] carousel public 폴더명 변경

--- react/src/pages/CarouselPage.tsx
+++ react/src/pages/CarouselPage.tsx
@@ -3,19 +3,19 @@ import { Carousel01, Carousel01Item } from '../components/carausel/Carousel01';
 const CarouselPage = () => {
   const cardList: Carousel01Item[] = [
     {
-      imgSrc: '/carousel-page/1.png',
+      imgSrc: '/carousel/1.png',
       title: 'item_01_title',
     },
     {
-      imgSrc: '/carousel-page/2.png',
+      imgSrc: '/carousel/2.png',
       title: 'item_02_title',
     },
     {
-      imgSrc: '/carousel-page/3.png',
+      imgSrc: '/carousel/3.png',
       title: 'item_03_title',
     },
     {
-      imgSrc: '/carousel-page/4.png',
+      imgSrc: '/carousel/4.png',
       title: 'item_04_title',
     },
   ];
