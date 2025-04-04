792b8a4243ce5aa260ccba48bfee9eba06b36d10
bkjNprosoft <bookj.nprosoft@gmail.com>
2025-04-04 12:10:03 +0900

    그래프 로직 수정

--- css-styling/various/src/pages/graph/index.tsx
+++ css-styling/various/src/pages/graph/index.tsx
@@ -2,7 +2,7 @@ import DoughnutGraph02 from "@/components/common/DoughnutGraph02";
 import { useGlobalNavOpenState } from "@/stores/store";
 import { useMemo, useState } from "react";
 import DoughnutGraph01 from "@/components/common/DoughnutGraph01";
-import { useCountUpEaseOutQuad } from "@/hooks/useCountUp";
+import { useCountUpEaseOutSine } from "@/hooks/useCountUp";
 
 const GraphPage = () => {
   const isOpen = useGlobalNavOpenState((state) => state.isOpen);
@@ -16,15 +16,14 @@ const GraphPage = () => {
 };
 
 const DoughnutGraphSection = () => {
-  const [inputStartCount, setInputStartCount] = useState(0);
   const [inputEndCount, setInputEndCount] = useState(100);
+  const [inputMaxCount, setInputMaxCount] = useState(100);
   const [inputDuration, setInputDuration] = useState(1000);
 
-  const { count, endCount, setPlay, setStartCount, setEndCount, setDuration } =
-    useCountUpEaseOutQuad({ start: 0, end: 100, durationSet: 1000 });
+  const { count, endCount, setPlay, setEndCount, setDuration } =
+    useCountUpEaseOutSine({ start: 0, end: 100, durationSet: 1000 });
 
   const handleStart = () => {
-    setStartCount(inputStartCount);
     setEndCount(inputEndCount);
     setDuration(inputDuration);
     setPlay(true);
@@ -32,8 +31,9 @@ const DoughnutGraphSection = () => {
 
   const percentage = useMemo(() => {
     if (count === 0) return 0;
-    return (count / endCount) * 100;
-  }, [count, endCount]);
+    const percent = (count / endCount) * 100;
+    return percent * (endCount / inputMaxCount);
+  }, [count, endCount, inputMaxCount]);
 
   return (
     <section className="relative flex flex-col justify-center items-center gap-[100px] py-[50px]">
@@ -41,7 +41,7 @@ const DoughnutGraphSection = () => {
         <DoughnutGraph01 percent={percentage} />
         <DoughnutGraph02
           percent={percentage}
-          text={`${Intl.NumberFormat("ko").format(count)}/${endCount}`}
+          text={`${Intl.NumberFormat("ko").format(count)}/${inputMaxCount}`}
         />
         <DoughnutGraph02
           barColor="#F44"
@@ -51,21 +51,21 @@ const DoughnutGraphSection = () => {
       </div>
       <div className="flex flex-col gap-[20px]">
         <div className="flex items-center gap-[10px]">
-          <label>Start Count</label>
-          <input
-            id="max-count"
-            className="border-[1px] border-blue-500 rounded-md text-[20px]"
-            value={inputStartCount}
-            onChange={(e) => setInputStartCount(Number(e.target.value))}
-          />
-        </div>
-        <div className="flex items-center gap-[10px]">
           <label>End Count</label>
           <input
             id="max-count"
             className="border-[1px] border-blue-500 rounded-md text-[20px]"
             value={inputEndCount}
             onChange={(e) => setInputEndCount(Number(e.target.value))}
+          />
+        </div>
+        <div className="flex items-center gap-[10px]">
+          <label>Max Count</label>
+          <input
+            id="max-count"
+            className="border-[1px] border-blue-500 rounded-md text-[20px]"
+            value={inputMaxCount}
+            onChange={(e) => setInputMaxCount(Number(e.target.value))}
           />
         </div>
         <div className="flex items-center gap-[10px]">
@@ -81,7 +81,13 @@ const DoughnutGraphSection = () => {
           className="p-[10px] border-[1px] border-blue-500 rounded-md text-[20px] hover:bg-blue-500 hover:text-white"
           onClick={() => handleStart()}
         >
-          start
+          Start
+        </button>
+        <button
+          className="p-[10px] border-[1px] border-blue-500 rounded-md text-[20px] hover:bg-blue-500 hover:text-white"
+          onClick={() => setPlay(false)}
+        >
+          Stop
         </button>
       </div>
     </section>
